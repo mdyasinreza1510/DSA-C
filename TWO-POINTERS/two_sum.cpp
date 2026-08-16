45a073e2123fec243c81172c3dc10150aae97603
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-16 20:25:01 +0530

    tryng bruteforce for 2 sum

---
+++ TWO-POINTERS/two_sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<climits>
+using namespace std;
+int main(){
+    int target=9;
+    int arr[5]={2,7,9,11,15};
+    for (int i =0; i<5 ; i++){
+        for (int j=1;j<5;j++){
+            if (arr[i]+arr[j] == target){
+                 break;
+                 
+            }
+        }
+    }
+
+}
