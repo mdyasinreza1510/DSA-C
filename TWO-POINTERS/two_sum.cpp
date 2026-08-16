cbf2f4971bc1e9160cfdef03e689f358aa01989a
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-16 23:23:37 +0530

    brute force method

--- TWO-POINTERS/two_sum.cpp
+++ TWO-POINTERS/two_sum.cpp
@@ -3,16 +3,18 @@
 #include<vector>
 #include<climits>
 using namespace std;
-int main(){
-    int target=9;
-    int arr[5]={2,7,9,11,15};
-    for (int i =0; i<5 ; i++){
-        for (int j=1;j<5;j++){
-            if (arr[i]+arr[j] == target){
-                 break;
+//BRUTE FORCE METHOD
+// int main(){
+//     int target=9;
+//     int arr[5]={2,3,7,11,15};
+//     for (int i =0; i<5 ; i++){
+//         for (int j=1;j<5;j++){
+//             if (arr[i]+arr[j] == target){
+//                 cout<< i <<endl; 
+//                 cout<< j;
                  
-            }
-        }
-    }
+//             }
+//         }
+//     }
 
-}
+// }
