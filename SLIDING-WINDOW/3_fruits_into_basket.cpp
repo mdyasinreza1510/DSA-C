1556858b65257106a90ed9fa4c0837553cac033f
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-27 23:55:11 +0530

    904 fruits into basket

--- SLIDING-WINDOW/3_fruits_into_basket.cpp
+++ SLIDING-WINDOW/3_fruits_into_basket.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int low=0,high=0,ans=INT_MIN;
-        unordered_map<int, int> mp;
+        unordered_map<int, int> mp; //sbse pehle ek map banaye jis me array ko trvaerse krte hue sare fruits ko add krenge 
+
         for(high=0;high<fruits.size();high++){
-            mp[fruits[high]]++;
-            while(mp.size() >2 ){
-                mp[fruits[low]]--;
-                if(mp[fruits[low]] == 0){
+            mp[fruits[high]]++;//sare fruits koo add krre hain eg:-[1,2,3]
+            while(mp.size() >2 ){//ye  check krega ki window me kitne distinct fruits hain mtlb kitne type k 
+                //to q k according agr disticnt element 2 se zyada hai to 
+                mp[fruits[low]]--; //low me jo fruit hsi usko km krdege [1,2,3] -> [0,2,3]
+                if(mp[fruits[low]] == 0){ // aur agar us low ka frequency 0 hogya to usko array se nikal denge
                     mp.erase(fruits[low]);
                 }
-                low++;
+                low++;//window ka size increase krdenge
             }
-            ans=max(ans,high-low+1);
+            ans=max(ans,high-low+1); //aur har window k size ko max se compare krke max size ka window nikal lenge 
         }
         return ans;
     }
