b1957fac8fb9d281dd9d92ebce334b59f5f04647
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-08-30 23:37:03 +0530

    .

--- SLIDING-WINDOW/5_replaceCharacyerWithameCharacter.cpp
+++ SLIDING-WINDOW/5_replaceCharacyerWithameCharacter.cpp
@@ -9,9 +9,9 @@ public:
         int maxFreq = 0;
 
         for (int high = 0; high < s.size(); high++) {
-            mp[s[high]]++;
-            maxFreq = max(maxFreq, mp[s[high]]);
-            while ((high - low + 1) - maxFreq > k) {
+            mp[s[high]]++; // sbse phle qidow me element add krte jarehain eg:- [a], [a,b], [a,b,a]
+            maxFreq = max(maxFreq, mp[s[high]]); /* ab yaha pe max frequency ko nikalte jayenge eg [a:2,b:1] maxfreq=2;*/ 
+            while ((high - low + 1) - maxFreq > k) { /* ab jab tak window ka size-max freq k se chota hai to window valid hai nahi to bada hone window shrink krenge */
                 mp[s[low]]--;
                 low++;
             }
