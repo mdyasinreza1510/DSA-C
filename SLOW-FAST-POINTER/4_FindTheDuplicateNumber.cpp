3eef1b7269afab357ece4b4d31abb33521cd97d8
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-02 23:49:05 +0530

    287 find the duplicate number

--- SLOW-FAST-POINTER/4_FindTheDuplicateNumber.cpp
+++ SLOW-FAST-POINTER/4_FindTheDuplicateNumber.cpp
@@ -1,21 +1,52 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+
+//         // Phase 1 : Cycle detect
+//         int slow = nums[0];
+//         int fast = nums[0];
+
+//         do {
+//             slow = nums[slow];              // 1 step
+//             fast = nums[nums[fast]];        // 2 steps
+//         } while (slow != fast);
+
+//         // Phase 2 : Find cycle starting point
+//         slow = nums[0];
+
+//         while (slow != fast) {
+//             slow = nums[slow];
+//             fast = nums[fast];
+//         }
+
+//         return slow;
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
 
-        // Phase 1 : Cycle detect
         int slow = nums[0];
         int fast = nums[0];
+        while (true) {
 
-        do {
-            slow = nums[slow];              // 1 step
-            fast = nums[nums[fast]];        // 2 steps
-        } while (slow != fast);
+            slow = nums[slow]; 
 
-        // Phase 2 : Find cycle starting point
+            fast = nums[nums[fast]];   
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+
         slow = nums[0];
 
         while (slow != fast) {
-            slow = nums[slow];
+
+            slow = nums[slow];         
+
             fast = nums[fast];
         }
 
