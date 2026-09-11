f79a03992a904744cf7ad1390897fe75342e2ddb
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-11 23:21:51 +0530

    leetcode 345 reverse vowels of a string

---
+++ TWO-POINTERS/20_reverseVowels.cpp
@@ -0,0 +1,23 @@
+//345
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int low=0,high=s.size()-1;
+        while(low<high){
+            while(low<high && !(s[low]=='a' ||s[low]=='e' ||s[low]=='i' ||s[low]=='o' ||s[low]=='u' ||s[low]=='A' ||s[low]=='E' ||s[low]=='I' ||s[low]=='O' ||s[low]=='U'))
+            {
+                low++;
+
+            }
+            while(low<high && !(s[high]=='a' ||s[high]=='e' ||s[high]=='i' ||s[high]=='o' ||s[high]=='u' ||s[high]=='A' ||s[high]=='E' ||s[high]=='I' ||s[high]=='O' ||s[high]=='U'))
+            {
+                high--;
+
+            }
+            swap(s[low],s[high]);
+            low++;
+            high--;
+        }
+        return s;
+    }
+};
