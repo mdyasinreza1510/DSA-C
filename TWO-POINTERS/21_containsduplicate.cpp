f91a565330f2c0b073d3a4b2344a34f9e337474c
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-12 22:33:15 +0530

    leetcode 217

---
+++ TWO-POINTERS/21_containsduplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0,j=i+1;
+        while(i<nums.size() && j<nums.size()){
+            if(nums[i]==nums[j]){
+                return true;
+            }
+            i++;
+            j++;
+        }
+        return false;
+        
+    }
+};
