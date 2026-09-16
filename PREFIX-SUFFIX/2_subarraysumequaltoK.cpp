f2ebdff690df5e4e9343d30ad250933c1d7d6808
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-16 23:35:07 +0530

    leetcode 560 subarraySum=k

---
+++ PREFIX-SUFFIX/2_subarraysumequaltoK.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+        unordered_map<int, int> mp;
+
+        mp[0] = 1;
+
+        int prefix = 0;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            prefix += nums[i];
+
+            if (mp.find(prefix - k) != mp.end()) {
+                ans += mp[prefix - k];
+            }
+
+            mp[prefix]++;
+        }
+
+        return ans;
+    }
+};
