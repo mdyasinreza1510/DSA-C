6d52d6f329003d303aa597232af09aaec5b8c116
mdyasinreza1510 <aec.cse.2024mdyasinreza@gmail.com>
2026-09-21 22:38:32 +0530

    leetcode 1991 find the middle index

--- PREFIX-SUFFIX/7_findmiddleIndex.cpp
+++ PREFIX-SUFFIX/7_findmiddleIndex.cpp
@@ -1,1 +1,20 @@
 //1991
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int left=0;
+        int sum=0;
+        for (int i=0;i<nums.size();i++){
+            sum=sum+nums[i];
+        }
+        for(int i=0; i<nums.size();i++){
+            int right= sum-nums[i]-left;
+            if (left == right){
+                return i;
+            }
+            left +=nums[i];
+        }
+        return -1;
+        
+    }
+};
